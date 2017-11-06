09ace4d21b525a76a389f3a57c7419bcfad05fd8
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-11-06 22:45:17 +0000

    add missing sln and unit test project

---
+++ test/unit/Framework.Cqrs.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Framework.Cqrs.Tests
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Best_Unit_Test_Ever()
+        {
+            Assert.True(true);
+        }
+    }
+}
